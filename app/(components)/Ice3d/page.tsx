723ea8edb10b56f06642976427085e0b2361aa52
kitish <kitishkumar2003@gmail.com>
2024-01-20 01:37:48 +0530

    added three

---
+++ app/(components)/Ice3d/page.tsx
@@ -0,0 +1,20 @@
+"use client";
+import { OrbitControls } from "@react-three/drei";
+import { Canvas } from "@react-three/fiber";
+import { Suspense } from "react";
+import { Icecream } from "./Icecream";
+
+export default function Ice3d() {
+  return (
+    <div className="h-[500px] sm:h-[700px] w-[100%] flex justify-center items-center">
+      <Canvas>
+        <OrbitControls enableZoom={false} />
+        <ambientLight intensity={0.5} />
+        <directionalLight position={[-2, 5, 2]} intensity={1} />
+        <Suspense fallback={null}>
+          <Icecream />
+        </Suspense>
+      </Canvas>
+    </div>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ import Hero from "./(components)/Hero/page";
 import ProductList from "./(components)/ProductList/page";
 import Tags from "./(components)/Tags/page";
 import Loading from "./loading";
+import Ice3d from "./(components)/Ice3d/page";
 
 export default function Home() {
   return (
@@ -13,6 +14,7 @@ export default function Home() {
         <ProductList />
       </Suspense>
       <Tags />
+      <Ice3d />
       <Faq />
     </div>
   );
