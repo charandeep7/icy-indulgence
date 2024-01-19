7c170f3afaf332217df3957c18d8817b7265726a
kitish <kitishkumar2003@gmail.com>
2024-01-20 05:12:07 +0530

    changed camera

--- app/(components)/Ice3d/page.tsx
+++ app/(components)/Ice3d/page.tsx
@@ -7,17 +7,13 @@ import { Html } from "next/document";
 import { useProgress } from "@nextui-org/react";
 
 export default function Ice3d() {
-  function Loader() {
-    const { active, progress, errors, item, loaded, total } = useProgress()
-    return <Html center>{progress} % loaded</Html>
-  }
   return (
     <div className="h-[500px] sm:h-[700px] w-[100%] flex justify-center items-center">
       <Canvas shadows={'soft'}>
         <OrbitControls enableZoom={false} />
         <ambientLight intensity={0.5} />
         <directionalLight position={[-2, 5, 2]} intensity={1} />
-        <Suspense fallback={<Loader />}>
+        <Suspense fallback={<h1 className="text-center z-50 text-black dark:text-white">Loading...</h1>}>
           <Icecream />
         </Suspense>
       </Canvas>
