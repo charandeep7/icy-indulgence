762450cfec5bc73acff6060775f6634b203d55ee
kitish <kitishkumar2003@gmail.com>
2024-01-29 15:52:47 +0530

    removed codes

--- app/api/user/getUserId.ts
+++ app/api/user/getUserId.ts
@@ -1,28 +1,5 @@
 import prisma from "../prisma/prismaClient";
 
-export const getUserIdByEmail = async (email: string) => {
-    try {
-        const id = await prisma.user.findUnique({
-            where: {
-                email
-            },
-            select: {
-                id: true,
-                createdAt: false,
-                email: false,
-                password: false,
-                updatedAt: false,
-                username: false
-            }
-        })
-        return id
-    } catch (error: any) {
-        throw new Error(error.message)
-    } finally {
-        prisma.$disconnect()
-    }
-}
-
 export const getUserIdDetail = async (name: string) => {
     try {
         const user = await prisma.user.findUnique({
