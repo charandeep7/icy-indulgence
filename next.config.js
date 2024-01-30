7cd4cb5b64e752053c8aae695a19a3111fe388de
kitish <kitishkumar2003@gmail.com>
2024-01-30 15:55:03 +0530

    added vanilla

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     transpilePackages: ['three'],
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'kitish-bucket.s3.ap-south-1.amazonaws.com',
+            }
+        ]
+    }
 }
 
 module.exports = nextConfig
